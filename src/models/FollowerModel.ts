64b4a500581a3f4681dde05e9dd6b3f73241e1ae
amasalov <delsol@bk.ru>
2021-05-14 10:12:13 +0300

    add models

---
+++ src/models/FollowerModel.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const FollowerSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    followers: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    following: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('Follower', FollowerSchema)
